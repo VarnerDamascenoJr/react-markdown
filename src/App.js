363037badb7e4a7a2adc0c9b63894b97bdce960c
Varner <varnerdjr@gmail.com>
2022-01-05 19:59:57 -0300

    final mod

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+import styled from 'styled-components'
+import MarkedInput from './components/MarkedInput';
+import Result from './components/Result';
+import EditorContext from './editorContext'
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const Title = styled.div`
+  font-size: 25px;
+  font-weight: 700;
+  font-family: "Lato", sans-serif;
+  margin-bottom: 1em;
+`
+const EditorContent = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;  
+` 
 
 function App() {
+  const [markedText, setMarkedText] = useState("")
+
+  const defaultValue = {
+    markedText,
+    setMarkedText
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <EditorContext.Provider  value={ defaultValue }>
+      <AppContainer>
+      <Title>React Markdown</Title>
+      <EditorContent>
+        <MarkedInput/>
+        <Result/>
+      </EditorContent>
+      </AppContainer>
+    </EditorContext.Provider>
   );
 }
 
